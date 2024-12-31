b5cb5dc82c8fbab228921acf5c754c3d001d59a2
aaleex777 <aaleex.777.010@gmail.com>
2024-12-31 21:24:17 +0100

    better gradient

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,20 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignUpPage'
-import CategoryPage from './pages/CategoryPage'
+import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import CategoryPage from './pages/CategoryPage';
+import Navbar from './components/Navbar';
+import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import { useUserStore } from './stores/useUserStore';
+import LoadingSpinner from './components/LoadingSpinner';
+import AdminPage from './pages/AdminPage';
+import CartPage from './pages/CartPage';
+import { useCartStore } from './stores/useCartStore';
+import PurchaseSuccessPage from './pages/PurchaseSuccessPage';
+import PurchaseCancelPage from './pages/PurchaseCancelPage';
 
-import Navbar from './components/Navbar'
-import { Toaster } from 'react-hot-toast'
-import { useEffect } from 'react'
-import { useUserStore } from './stores/useUserStore'
-import LoadingSpinner from './components/LoadingSpinner'
-import AdminPage from './pages/AdminPage'
-import CartPage from './pages/CartPage'
-import { useCartStore } from './stores/useCartStore'
-import PurchaseSuccessPage from './pages/PurchaseSuccessPage'
-import PurchaseCancelPage from './pages/PurchaseCancelPage'
 function App() {
-
   const { user, checkAuth, checkingAuth } = useUserStore();
   const { getCartItems } = useCartStore();
 
@@ -26,20 +25,20 @@ function App() {
   useEffect(() => {
     if (!user) return;
     getCartItems();
-  }, [getCartItems, user])
+  }, [getCartItems, user]);
 
   if (checkingAuth) return <LoadingSpinner />;
+
   return (
     <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
-
       <div className='absolute inset-0 overflow-hidden'>
-        <div className='absolute inset-0'>
-          <div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,#1C6E8C_0%,#274156_100%)]' />
+        <div className='absolute inset-0 animate-gradient'>
+          <div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,#1C6E8C_0%,#274156_50%,#1B3A4B_100%)] opacity-70' />
+          <div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_bottom,#1C6E8C_0%,#274156_50%,#1B3A4B_100%)] opacity-70' />
         </div>
       </div>
 
       <div className='relative z-50 pt-20'>
-
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -48,8 +47,8 @@ function App() {
           <Route path='/admin-dashboard' element={user?.role === "admin" ? <AdminPage /> : <Navigate to='/login' />} />
           <Route path="category/:category" element={<CategoryPage />} />
           <Route path="cart" element={user ? <CartPage /> : <Navigate to="/login" />} />
-          <Route path='/purchase-success' element={user ? <PurchaseSuccessPage /> : <Navigate to="/login" />}/>
-          <Route path='/purchase-cancel' element={user ? <PurchaseCancelPage /> : <Navigate to="/login" />}/>
+          <Route path='/purchase-success' element={user ? <PurchaseSuccessPage /> : <Navigate to="/login" />} />
+          <Route path='/purchase-cancel' element={user ? <PurchaseCancelPage /> : <Navigate to="/login" />} />
         </Routes>
       </div>
       <Toaster />
@@ -57,4 +56,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        animation: {
+          gradient: 'gradient 10s ease-in-out infinite', // Define the animation
+        },
+        keyframes: {
+          gradient: {
+            '0%': { transform: 'scale(1)' },
+            '50%': { transform: 'scale(1.05)' },
+            '100%': { transform: 'scale(1)' },
+          },
+        },
+    },
   },
   plugins: [],
 }
